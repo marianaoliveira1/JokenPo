2bf4d1f33bd81bd74fe2db23f92affedc8aee4ca
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2023-03-08 19:29:35 -0300

    ui

--- lib/screens/jogo_screen.dart
+++ lib/screens/jogo_screen.dart
@@ -15,7 +15,7 @@ class _JogoScreenState extends State<JogoScreen> {
         title: Text("JokenPo"),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(
             padding: EdgeInsets.only(top: 32, bottom: 16),
@@ -23,6 +23,35 @@ class _JogoScreenState extends State<JogoScreen> {
               "Escolha do App",
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Image.asset("images/pedra.png"),
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha uma opção abaixo",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset(
+                  "images/papel.png",
+                  height: 110,
+                ),
+                Image.asset(
+                  "images/pedra.png",
+                  height: 110,
+                ),
+                Image.asset(
+                  "images/tesoura.png",
+                  height: 110,
+                ),
+              ],
             ),
           )
         ],
